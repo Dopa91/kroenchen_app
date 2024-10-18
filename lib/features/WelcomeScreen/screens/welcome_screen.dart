476aa4abce41b0d2812c89e114514bc9728f4c6e
Dopa91 <code.dopatka@gmail.com>
2024-10-18 15:59:04 +0200

    moved extracted widget: WelcomeScreen to folder

---
+++ lib/features/WelcomeScreen/screens/welcome_screen.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:kroenchen_app/features/shared/my_individual_button.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 12),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Kroenchen",
+                  style: TextStyle(
+                    fontSize: 32,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 32),
+            Center(
+              child: Image.asset(
+                "assets/images/k_logo.png",
+                height: 512,
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 32),
+              child: Text(
+                "Herzlich Willkommen",
+                style: TextStyle(
+                  fontSize: 32,
+                ),
+              ),
+            ),
+            const SizedBox(height: 32),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24),
+              child: MyIndividualButton(
+                newText: 'Reise Starten',
+                nextSite: () => Navigator.pushNamed(context, "/loginpage"),
+                icon: Icons.arrow_forward,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
