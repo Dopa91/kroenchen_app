55db25e3e39b20ff33e8584be074f5ac9f67d541
Dopa91 <code.dopatka@gmail.com>
2024-10-18 22:32:36 +0200

    changed mainAxisAlig to end

--- lib/features/Homepage/screens/settings_screen.dart
+++ lib/features/Homepage/screens/settings_screen.dart
@@ -6,31 +6,38 @@ class SettingsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage("assets/images/k_logo.png"),
+    return Padding(
+      padding: const EdgeInsets.all(24.0),
+      child: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/k_logo.png"),
+          ),
         ),
-      ),
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Column(
-              children: [
-                MyIndividualButton(
-                  newText: "Logout",
-                  nextSite: () => Navigator.pushNamed(context, "/loginscreen"),
-                  icon: Icons.logout,
-                ),
-                const MyIndividualButton(
-                  newText: "Konto Löschen",
-                  nextSite: null,
-                  icon: Icons.delete_forever,
-                )
-              ],
-            )
-          ],
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Column(
+                children: [
+                  MyIndividualButton(
+                    newText: "Logout",
+                    nextSite: () =>
+                        Navigator.pushNamed(context, "/loginscreen"),
+                    icon: Icons.logout,
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  const MyIndividualButton(
+                    newText: "Konto Löschen",
+                    nextSite: null,
+                    icon: Icons.delete_forever,
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
