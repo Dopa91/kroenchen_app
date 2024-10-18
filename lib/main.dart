43b4fa7d5c8da876f6952d9e06f22630ad72e699
Dopa91 <code.dopatka@gmail.com>
2024-10-18 21:20:53 +0200

    added routes to screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:kroenchen_app/config/themes.dart';
+import 'package:kroenchen_app/features/Homepage/screens/bottom_Navigation_bar_screen.dart';
 import 'package:kroenchen_app/features/Login/screens/loginpage.dart';
+import 'package:kroenchen_app/features/SignUp/screens/register_screen.dart';
+import 'package:kroenchen_app/features/SignUp/screens/register_screen_finish.dart';
+import 'package:kroenchen_app/features/SignUp/screens/register_screen_three.dart';
+import 'package:kroenchen_app/features/SignUp/screens/register_screen_two.dart';
 import 'package:kroenchen_app/features/WelcomeScreen/screens/welcome_screen.dart';
 
 void main() {
@@ -17,7 +22,11 @@ class MainApp extends StatelessWidget {
       home: const WelcomeScreen(),
       routes: {
         '/loginscreen': (context) => const LoginScreen(),
-        // '/registerscreenstart': (context) => const RegisterScreenStart(),
+        '/registerscreenstart': (context) => const RegisterScreenStart(),
+        '/registerscreentwo': (context) => const RegisterScreenTwo(),
+        '/registerscreenthree': (context) => const RegisterScreenThree(),
+        '/registerscreenfinish': (context) => const RegisterScreenFinish(),
+        '/homepage': (context) => const BottomNavigationBarScreen(),
       },
     );
   }
