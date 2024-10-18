8007e359a26adb8c41433f387460f2a461640f3c
Dopa91 <code.dopatka@gmail.com>
2024-10-18 14:59:43 +0200

    added Fontfamily "Outfit" and added it in Class Theme

--- lib/config/themes.dart
+++ lib/config/themes.dart
@@ -1,19 +1,37 @@
 import 'package:flutter/material.dart';
 
 final ThemeData lightTheme = ThemeData(
-  fontFamily: "Outfit",
+  fontFamily: "Roboto",
   textTheme: const TextTheme(
-    bodyMedium: TextStyle(fontSize: 21),
-    headlineMedium: TextStyle(fontSize: 30),
-    bodySmall: TextStyle(fontSize: 16),
+    bodyMedium: TextStyle(
+      fontSize: 18,
+      fontFamily: "Outfit",
+    ),
+    headlineMedium: TextStyle(
+      fontSize: 30,
+      fontFamily: "Outfit",
+    ),
+    bodySmall: TextStyle(
+      fontSize: 16,
+      fontFamily: "Outfit",
+    ),
   ),
 );
 
 final ThemeData darkTheme = ThemeData(
   fontFamily: "Outfit",
   textTheme: const TextTheme(
-    bodyMedium: TextStyle(fontSize: 21),
-    headlineMedium: TextStyle(fontSize: 42),
-    bodySmall: TextStyle(fontSize: 14),
+    bodyMedium: TextStyle(
+      fontSize: 21,
+      fontFamily: "Outfit",
+    ),
+    headlineMedium: TextStyle(
+      fontSize: 42,
+      fontFamily: "Outfit",
+    ),
+    bodySmall: TextStyle(
+      fontSize: 14,
+      fontFamily: "Outfit",
+    ),
   ),
 );
