2f6701ee709870834ce595249ca594ae489d6d8f
Dopa91 <code.dopatka@gmail.com>
2024-10-18 21:24:28 +0200

    added Diary screen to bottomNavigationBar

---
+++ lib/features/Homepage/screens/diary_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class DiaryScreen extends StatelessWidget {
+  const DiaryScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/images/k_logo.png"),
+            ),
+          ),
+          child: const Column(
+            children: [
+              Center(
+                child: Column(
+                  children: [
+                    Text(
+                      "Hier kommt die Tagebuchfunktion rein, mit übersicht und eintrag erstellen",
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
