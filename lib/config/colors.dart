05393d190655582b00a1f7f58ee6e8504bd27761
Dopa91 <code.dopatka@gmail.com>
2024-10-18 15:37:28 +0200

    created config class colors

---
+++ lib/config/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+Color backgroundColorPurple = const Color.fromARGB(255, 160, 81, 238);
+Color brighterPurple = const Color.fromARGB(255, 217, 192, 242);
+Color buttonBlue = const Color.fromARGB(255, 16, 98, 219);
