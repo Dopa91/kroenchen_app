d0c757eb8d67f591f71a339e4c85f635efbde397
Dopa91 <code.dopatka@gmail.com>
2024-10-18 15:38:25 +0200

    created WelcomeScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:kroenchen_app/config/themes.dart';
+import 'package:kroenchen_app/features/shared/my_individual_button.dart';
 
 void main() {
   runApp(const MainApp());
@@ -12,9 +13,50 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: lightTheme,
-      home: const Scaffold(
-        body: Center(
-          child: Text('Hello World!,ich teste es nur '),
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 12),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Kroenchen",
+                    style: TextStyle(
+                      fontSize: 32,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 32),
+              Center(
+                child: Image.asset(
+                  "assets/images/k_logo.png",
+                  height: 512,
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 32),
+                child: Text(
+                  "Herzliche Willkommen",
+                  style: TextStyle(
+                    fontSize: 32,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 32),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                child: MyIndividualButton(
+                  newText: 'Reise Starten',
+                  nextSite: () => Navigator.pushNamed(context, "/loginpage"),
+                  icon: Icons.arrow_forward,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
