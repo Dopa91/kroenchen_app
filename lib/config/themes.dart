6e63078c14c336c73caa4cadafcfca82b3828f86
Dopa91 <code.dopatka@gmail.com>
2024-10-21 12:12:10 +0200

    fotmated code for better Overview

--- lib/config/themes.dart
+++ lib/config/themes.dart
@@ -2,34 +2,45 @@ import 'package:flutter/material.dart';
 import 'package:kroenchen_app/config/colors.dart';
 
 final ThemeData lightTheme = ThemeData(
-    fontFamily: "Outfit",
-    textTheme: TextTheme(
-      bodyMedium:
-          TextStyle(fontSize: 18, fontFamily: "Outfit", color: brighterPurple),
-      headlineMedium:
-          TextStyle(fontSize: 42, fontFamily: "Outfit", color: brighterPurple),
-      bodySmall:
-          TextStyle(fontSize: 14, fontFamily: "Outfit", color: brighterPurple),
+  fontFamily: "Outfit",
+  textTheme: TextTheme(
+    bodyMedium: TextStyle(
+      fontSize: 18,
+      fontFamily: "Outfit",
+      color: brighterPurple,
     ),
-    scaffoldBackgroundColor: backgroundColorPurple);
+    headlineMedium: TextStyle(
+      fontSize: 42,
+      fontFamily: "Outfit",
+      color: brighterPurple,
+    ),
+    bodySmall: TextStyle(
+      fontSize: 14,
+      fontFamily: "Outfit",
+      color: brighterPurple,
+    ),
+  ),
+  scaffoldBackgroundColor: backgroundColorPurple,
+);
 
 final ThemeData darkTheme = ThemeData(
-    fontFamily: "Outfit",
-    textTheme: const TextTheme(
-      bodyMedium: TextStyle(
-        fontSize: 18,
-        fontFamily: "Outfit",
-        color: Colors.white,
-      ),
-      headlineMedium: TextStyle(
-        fontSize: 42,
-        fontFamily: "Outfit",
-        color: Colors.white,
-      ),
-      bodySmall: TextStyle(
-        fontSize: 14,
-        fontFamily: "Outfit",
-        color: Colors.white,
-      ),
+  fontFamily: "Outfit",
+  textTheme: const TextTheme(
+    bodyMedium: TextStyle(
+      fontSize: 18,
+      fontFamily: "Outfit",
+      color: Colors.white,
     ),
-    scaffoldBackgroundColor: backgroundColorPurple);
+    headlineMedium: TextStyle(
+      fontSize: 42,
+      fontFamily: "Outfit",
+      color: Colors.white,
+    ),
+    bodySmall: TextStyle(
+      fontSize: 14,
+      fontFamily: "Outfit",
+      color: Colors.white,
+    ),
+  ),
+  scaffoldBackgroundColor: backgroundColorPurple,
+);
