9e090aab91ab72461911a7f098b9ec781fadd47f
Dopa91 <code.dopatka@gmail.com>
2024-10-22 15:43:10 +0200

    formated, for better overview

--- lib/config/colors.dart
+++ lib/config/colors.dart
@@ -4,5 +4,4 @@ Color backgroundColorPurple = const Color.fromARGB(255, 160, 81, 238);
 Color brighterPurple = const Color.fromARGB(255, 217, 192, 242);
 Color buttonBlue = const Color.fromARGB(255, 16, 98, 219);
 Color darkerButtonBlue = const Color.fromARGB(255, 8, 52, 117);
-
 Color brighterButtonBlue = const Color.fromARGB(255, 116, 145, 230);
