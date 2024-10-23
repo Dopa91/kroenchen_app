76a0655a2a1bce2841df1aa7a5c2ffb3cddbb112
Dopa91 <code.dopatka@gmail.com>
2024-10-24 00:02:30 +0200

    edited theme file. Added new Colors for scaffoldBackgroundColor

--- lib/config/themes.dart
+++ lib/config/themes.dart
@@ -2,27 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:kroenchen_app/config/colors.dart';
 
 final ThemeData darkTheme = ThemeData(
-    fontFamily: "Outfit",
-    textTheme: TextTheme(
-      bodyMedium: TextStyle(
-        fontSize: 18,
-        fontFamily: "Outfit",
-        color: brighterPurple,
-      ),
-      headlineMedium: TextStyle(
-        fontSize: 42,
-        fontFamily: "Outfit",
-        color: brighterPurple,
-      ),
-      bodySmall: TextStyle(
-        fontSize: 14,
-        fontFamily: "Outfit",
-        color: brighterPurple,
-      ),
+  fontFamily: "Outfit",
+  textTheme: TextTheme(
+    bodyMedium: TextStyle(
+      fontSize: 18,
+      fontFamily: "Outfit",
+      color: brighterPurple,
     ),
-    navigationBarTheme:
-        NavigationBarThemeData(backgroundColor: darkerBrighterPurple),
-    scaffoldBackgroundColor: Colors.transparent);
+    headlineMedium: TextStyle(
+      fontSize: 42,
+      fontFamily: "Outfit",
+      color: brighterPurple,
+    ),
+    bodySmall: TextStyle(
+      fontSize: 14,
+      fontFamily: "Outfit",
+      color: brighterPurple,
+    ),
+  ),
+  navigationBarTheme:
+      NavigationBarThemeData(backgroundColor: darkerBrighterPurple),
+  scaffoldBackgroundColor: Colors.transparent,
+  // scaffoldBackgroundColor: darkerBackgroundColorPurple,
+);
 
 final ThemeData lightTheme = ThemeData(
   fontFamily: "Outfit",
@@ -46,4 +48,5 @@ final ThemeData lightTheme = ThemeData(
   // primaryColor: backgroundColorPurple,
   navigationBarTheme: NavigationBarThemeData(backgroundColor: brighterPurple),
   scaffoldBackgroundColor: Colors.transparent,
+  // scaffoldBackgroundColor: backgroundColorPurple,
 );
