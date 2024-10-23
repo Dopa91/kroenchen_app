41c7b05c10b62d69e46b6dd0034029a5193acc69
Dopa91 <code.dopatka@gmail.com>
2024-10-23 14:21:16 +0200

    added two final for SwitchButton Dark/lightMode in Settingsscreen

--- lib/features/Homepage/screens/bottom_Navigation_bar_main.dart
+++ lib/features/Homepage/screens/bottom_Navigation_bar_main.dart
@@ -5,7 +5,11 @@ import 'package:kroenchen_app/features/Homepage/screens/homepage.dart';
 import 'package:kroenchen_app/features/Homepage/screens/settings_screen.dart';
 
 class BottomNavigationBarMain extends StatefulWidget {
-  const BottomNavigationBarMain({super.key});
+  const BottomNavigationBarMain(
+      {super.key, required this.isSwitched, required this.onChanged});
+
+  final bool isSwitched;
+  final void Function(bool) onChanged;
 
   @override
   State<BottomNavigationBarMain> createState() =>
@@ -13,13 +17,19 @@ class BottomNavigationBarMain extends StatefulWidget {
 }
 
 class _BottomNavigationBarMainState extends State<BottomNavigationBarMain> {
-  final List<Widget> widgets = [
-    const Homepage(),
-    const DiaryScreen(),
-    const SettingsScreen(),
-  ];
+  late List<Widget> widgets;
 
   int currentIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    widgets = [
+      const Homepage(),
+      const DiaryScreen(),
+      SettingsScreen(isSwitched: widget.isSwitched),
+    ];
+  }
 
   @override
   Widget build(BuildContext context) {
