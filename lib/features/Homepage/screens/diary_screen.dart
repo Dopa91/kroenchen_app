6290c4e3694172ac27e96d581e377a2f5f2039a0
Dopa91 <code.dopatka@gmail.com>
2024-10-24 15:27:04 +0200

    removed Container with Image, &added new with over Scaffold.

--- lib/features/Homepage/screens/diary_screen.dart
+++ lib/features/Homepage/screens/diary_screen.dart
@@ -1,55 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:kroenchen_app/config/colors.dart';
+import 'package:kroenchen_app/features/shared/background_image_widget.dart';
 
 class DiaryScreen extends StatelessWidget {
   const DiaryScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: buttonBlue,
-        mini: true,
-        onPressed: () {
-          showModalBottomSheet(
-            context: context,
-            builder: (BuildContext context) {
-              return const SizedBox(
-                height: 256,
-                width: 256,
-                child: Center(
-                  child: Text("Hello"),
-                ),
-              );
-            },
-          );
-        },
-        child: Icon(
-          Icons.add,
-          color: brighterPurple,
-        ),
-      ),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(24.0),
-          child: Container(
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage("assets/images/k_logo.png"),
-              ),
-            ),
-            child: const Column(
-              children: [
-                Center(
-                  child: Column(
-                    children: [
-                      Text(
-                        "Hier kommt die Tagebuchfunktion rein, mit übersicht und eintrag erstellen",
-                      ),
-                    ],
+    return BackgroundImageWidget(
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: buttonBlue,
+          mini: true,
+          onPressed: () {
+            showModalBottomSheet(
+              context: context,
+              builder: (BuildContext context) {
+                return const SizedBox(
+                  height: 256,
+                  width: 256,
+                  child: Center(
+                    child: Text("Hello"),
                   ),
-                ),
-              ],
+                );
+              },
+            );
+          },
+          child: Icon(
+            Icons.add,
+            color: brighterPurple,
+          ),
+        ),
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(24.0),
+            child: Container(
+              decoration: const BoxDecoration(),
+              child: const Column(
+                children: [
+                  Center(
+                    child: Column(
+                      children: [
+                        Text(
+                          "Hier kommt die Tagebuchfunktion rein, mit übersicht und eintrag erstellen",
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
--- lib/features/Homepage/screens/homepage.dart
+++ lib/features/Homepage/screens/homepage.dart
@@ -1,42 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:kroenchen_app/features/Homepage/widgets/profil_account_picture.dart';
+import 'package:kroenchen_app/features/shared/background_image_widget.dart';
 
 class Homepage extends StatelessWidget {
   const Homepage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(
-            24.0,
-          ),
-          child: Container(
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage("assets/images/k_logo.png"),
-              ),
+    return BackgroundImageWidget(
+      child: Scaffold(
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(
+              24.0,
             ),
-            child: const Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  children: [
-                    ProfilAccountPicture(),
-                  ],
-                ),
-                SizedBox(
-                  height: 16,
-                ),
-                Text(
-                  "Willkommen,",
-                ),
-                Text(
-                  "MusterNutzer",
-                ),
-                Text("Placeholder für Kalendar"),
-              ],
+            child: Container(
+              child: const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      ProfilAccountPicture(),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  Text(
+                    "Willkommen,",
+                  ),
+                  Text(
+                    "MusterNutzer",
+                  ),
+                  Text("Placeholder für Kalendar"),
+                ],
+              ),
             ),
           ),
         ),
