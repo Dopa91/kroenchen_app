0e871d5c0e264d2f0849b58ca74dd6cd0ee35ca3
Dopa91 <code.dopatka@gmail.com>
2024-10-24 17:09:43 +0200

    added BackgroundImageWidget & set image "null"

--- lib/features/Login/screens/loginpage.dart
+++ lib/features/Login/screens/loginpage.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:ionicons/ionicons.dart';
 import 'package:kroenchen_app/features/Login/widgets/sign_in_divider.dart';
 import 'package:kroenchen_app/features/Login/widgets/sign_up_icon_button.dart';
+import 'package:kroenchen_app/features/shared/background_image_widget.dart';
 import 'package:kroenchen_app/features/shared/my_individual_button.dart';
 import 'package:kroenchen_app/features/shared/textfield_with_border.dart';
 
@@ -10,75 +11,78 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        forceMaterialTransparency: true,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.only(
-          left: 24,
-          right: 24,
+    return BackgroundImageWidget(
+      image: null,
+      child: Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          forceMaterialTransparency: true,
         ),
-        child: Column(
-          children: [
-            Image.asset("assets/images/k_logo.png"),
-            const Row(
-              children: [
-                SizedBox(
-                  width: 8,
-                ),
-                Text(
-                  "Login \nMelde dich jetzt an",
-                ),
-              ],
-            ),
-            const TextFieldBox(
-              text: 'Email',
-            ),
-            const TextFieldBox(
-              text: 'Password',
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                top: 72,
+        body: Padding(
+          padding: const EdgeInsets.only(
+            left: 24,
+            right: 24,
+          ),
+          child: Column(
+            children: [
+              Image.asset("assets/images/k_logo.png"),
+              const Row(
+                children: [
+                  SizedBox(
+                    width: 8,
+                  ),
+                  Text(
+                    "Login \nMelde dich jetzt an",
+                  ),
+                ],
               ),
-              child: MyIndividualButton(
-                newText: "Login",
-                nextSite: () => Navigator.pushNamed(context, "/homepage"),
-                icon: Icons.arrow_forward,
+              const TextFieldBox(
+                text: 'Email',
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(
-                top: 12,
+              const TextFieldBox(
+                text: 'Password',
               ),
-              child: MyIndividualButton(
-                newText: "Registrieren",
-                nextSite: () =>
-                    Navigator.pushNamed(context, "/registerscreenstart"),
-                icon: Icons.arrow_forward,
+              Padding(
+                padding: const EdgeInsets.only(
+                  top: 72,
+                ),
+                child: MyIndividualButton(
+                  newText: "Login",
+                  nextSite: () => Navigator.pushNamed(context, "/homepage"),
+                  icon: Icons.arrow_forward,
+                ),
               ),
-            ),
-            const Padding(
-              padding: EdgeInsets.all(16),
-              child: SignInDivider(),
-            ),
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SignUpIconButton(
-                  icon: Ionicons.logo_apple,
+              Padding(
+                padding: const EdgeInsets.only(
+                  top: 12,
                 ),
-                SizedBox(
-                  width: 8,
+                child: MyIndividualButton(
+                  newText: "Registrieren",
+                  nextSite: () =>
+                      Navigator.pushNamed(context, "/registerscreenstart"),
+                  icon: Icons.arrow_forward,
                 ),
-                SignUpIconButton(
-                  icon: Ionicons.logo_google,
-                ),
-              ],
-            )
-          ],
+              ),
+              const Padding(
+                padding: EdgeInsets.all(16),
+                child: SignInDivider(),
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SignUpIconButton(
+                    icon: Ionicons.logo_apple,
+                  ),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  SignUpIconButton(
+                    icon: Ionicons.logo_google,
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
