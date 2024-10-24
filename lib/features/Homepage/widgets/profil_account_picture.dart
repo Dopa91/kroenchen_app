b108035f4065dee042bb68f5411604ac2dcadc50
Dopa91 <code.dopatka@gmail.com>
2024-10-24 23:38:52 +0200

    added var radius

--- lib/features/Homepage/widgets/profil_account_picture.dart
+++ lib/features/Homepage/widgets/profil_account_picture.dart
@@ -3,12 +3,15 @@ import 'package:flutter/material.dart';
 class ProfilAccountPicture extends StatelessWidget {
   const ProfilAccountPicture({
     super.key,
+    this.radius = 60,
   });
+
+  final double radius;
 
   @override
   Widget build(BuildContext context) {
-    return const CircleAvatar(
-      radius: 60,
+    return CircleAvatar(
+      radius: radius,
       backgroundImage: AssetImage("assets/images/bober.jpg"),
     );
   }
