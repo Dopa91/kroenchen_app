c40ea77b7ba5c9ae426c06e1dbd06a52a24e84a2
Dopa91 <code.dopatka@gmail.com>
2024-10-31 09:42:20 +0100

    better overview

--- lib/config/colors.dart
+++ lib/config/colors.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 
-Color backgroundColorPurple = const Color.fromARGB(255, 160, 81, 238);
-Color darkerBackgroundColorPurple = const Color.fromARGB(255, 75, 0, 150);
 Color brighterPurple = const Color.fromARGB(255, 217, 192, 242);
 Color darkerBrighterPurple = const Color.fromARGB(255, 194, 151, 237);
+
 Color buttonBlue = const Color.fromARGB(255, 16, 98, 219);
 Color darkerButtonBlue = const Color.fromARGB(255, 8, 52, 117);
 Color brighterButtonBlue = const Color.fromARGB(255, 116, 145, 230);
+
 Color generalWhite = Color.fromARGB(255, 222, 226, 233);
+
+Color darkerBackgroundColorPurple = const Color.fromARGB(255, 75, 0, 150);
+Color backgroundColorPurple = const Color.fromARGB(255, 160, 81, 238);
