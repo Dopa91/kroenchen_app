7c4243772ec3577486679fbce59c20cdc1f375d9
Dopa91 <code.dopatka@gmail.com>
2024-10-31 18:09:17 +0100

    crated class for diaryEntry

---
+++ lib/features/repository/models/diary.dart
@@ -0,0 +1,13 @@
+class DiaryEntry {
+  String date;
+  String content;
+  bool hasFever;
+  int painScale;
+
+  DiaryEntry({
+    required this.date,
+    required this.content,
+    required this.hasFever,
+    required this.painScale,
+  });
+}
