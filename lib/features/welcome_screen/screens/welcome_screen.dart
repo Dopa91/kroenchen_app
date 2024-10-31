3792f2c47de0259326ec9e24e06535fb6a516e40
Dopa91 <code.dopatka@gmail.com>
2024-10-31 12:15:56 +0100

    edited screen. extracted widget

--- lib/features/welcome_screen/screens/welcome_screen.dart
+++ lib/features/welcome_screen/screens/welcome_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:kroenchen_app/features/shared/background_image_widget.dart';
-import 'package:kroenchen_app/features/shared/my_individual_button.dart';
+import 'package:kroenchen_app/features/welcome_screen/widgets/welcome_screen_content.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({
@@ -11,44 +11,7 @@ class WelcomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return BackgroundImageWidget(
       child: Scaffold(
-        body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(24.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                const SizedBox(height: 12),
-                const Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      "",
-                      style: TextStyle(
-                        fontSize: 32,
-                      ),
-                    ),
-                  ],
-                ),
-                Expanded(child: const SizedBox(height: 32)),
-                const Text(
-                  "Herzlich Willkommen",
-                  style: TextStyle(
-                    fontSize: 32,
-                  ),
-                ),
-                const SizedBox(height: 32),
-                MyIndividualButton(
-                  newText: 'Reise Starten',
-                  nextSite: () => Navigator.pushNamed(context, "/loginscreen"),
-                  icon: Icons.arrow_forward,
-                ),
-                SizedBox(
-                  height: 40,
-                )
-              ],
-            ),
-          ),
-        ),
+        body: WelcomeScreenContent(),
       ),
     );
   }
