25d6b31a730e67b574b03508b205d77d4d4ef18e
Dopa91 <code.dopatka@gmail.com>
2024-12-04 14:22:11 +0100

    Cleaned DatabaseRepository & added function getDiaryEntries

--- lib/shared/repository/database_repository.dart
+++ lib/shared/repository/database_repository.dart
@@ -3,28 +3,18 @@ import 'package:kroenchen_app/shared/models/diary.dart';
 import 'package:kroenchen_app/shared/models/user.dart';
 
 abstract class DatabaseRepository {
-// Einen neuen Benutzer hinzufügen (createUser)
   Future<void> createUser(User user);
-// Bestehende Benutzerdaten bearbeiten ( editUser)
   Future<void> editUser(User user);
-// Benutzerdaten löschen (deleteUser)
   Future<void> deleteUser(User user);
 
-// Einen neuen Tagebucheintrag erstellen (createDiaryEntry)
   Future<void> createDiaryEntry(DiaryEntry diaryEntry);
-// Den Tagebucheintrag anzeigen lassen (showDiaryEntry)
   Future<DiaryEntry> showDiaryEntry(DiaryEntry diaryEntry);
-// Tagebucheintrag bearbeiten ( editDiaryEntry)
   Future<void> editDiaryEntry(DiaryEntry diaryEntry);
-// Tagebucheintrag löschen (deleteDiaryEntry)
   Future<void> deleteDiaryEntry(DiaryEntry diaryEntry);
+  Future<List<DiaryEntry>> getDiaryEntries();
 
-// Einen neuen Termineintrag erstellen (createAppointment)
   Future<void> createAppointment(Appointment appointment);
-// Den erstellten Termin  anzeigen lassen (showAppointment)
   Future<Appointment> showAppointment(Appointment appointment);
-// Termine bearbeiten ( editAppointment)
   Future<void> editAppointment(Appointment appointment);
-// Termin löschen (deleteAppointment)
   Future<void> deleteAppointment(Appointment appointment);
 }
