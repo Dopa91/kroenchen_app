62942fa375c9a28f3c99bb418d39f6345601851d
Dopa91 <code.dopatka@gmail.com>
2024-12-04 20:47:19 +0100

    expanded class Diary, to Save Data in SharedPreferencesDatabase

--- lib/shared/models/diary.dart
+++ lib/shared/models/diary.dart
@@ -10,4 +10,22 @@ class DiaryEntry {
     required this.hasFever,
     required this.pain,
   });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'date': date.toIso8601String(),
+      'content': content,
+      'hasFever': hasFever,
+      'pain': pain,
+    };
+  }
+
+  static DiaryEntry fromJson(Map<String, dynamic> json) {
+    return DiaryEntry(
+      date: DateTime.parse(json['date']),
+      content: json['content'],
+      hasFever: json['hasFever'],
+      pain: json['pain'],
+    );
+  }
 }
