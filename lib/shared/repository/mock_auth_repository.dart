37c49e7df07b3a1caec0db0f399a8cb052fa48b3
Dopa91 <code.dopatka@gmail.com>
2024-12-16 23:39:37 +0100

    Created Mock Auth for Firebase login

---
+++ lib/shared/repository/mock_auth_repository.dart
@@ -0,0 +1,48 @@
+import 'dart:async';
+import 'package:kroenchen_app/shared/models/user.dart' as app_models;
+import 'package:kroenchen_app/shared/repository/auth_repository.dart';
+
+class MockAuthRepository implements AuthRepository {
+  app_models.AppUser? actualUser;
+  final controller = StreamController<app_models.AppUser?>.broadcast();
+
+  MockAuthRepository() {
+    controller.add(actualUser);
+  }
+
+  @override
+  Stream<app_models.AppUser?> get authStateChanges => controller.stream;
+
+  @override
+  app_models.AppUser? get currentUser => actualUser;
+
+  @override
+  Future<app_models.AppUser?> signInWithEmailAndPassword(
+      String email, String password) async {
+    actualUser = app_models.AppUser(userId: "bober", email: email);
+    controller.add(actualUser);
+    return actualUser;
+  }
+
+  @override
+  Future<app_models.AppUser?> createUserWithEmailAndPassword(
+      String email, String password) async {
+    actualUser = app_models.AppUser(userId: 'bobermock', email: email);
+    controller.add(actualUser);
+    return actualUser;
+  }
+
+  @override
+  Future<void> signOut() async {
+    actualUser = null;
+    controller.add(actualUser);
+  }
+
+  @override
+  Future<app_models.AppUser?> signInWithGoogle() async {
+    actualUser =
+        app_models.AppUser(userId: 'bobrtGoogle', email: 'boberMock@bober.com');
+    controller.add(actualUser);
+    return actualUser;
+  }
+}
