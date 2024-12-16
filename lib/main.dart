2b7f720a5179ac651b97b90ff1e2ceec4ac14ae8
Dopa91 <code.dopatka@gmail.com>
2024-12-16 23:08:47 +0100

    Adjusted MultiProvider to use Shared&Firebase-Repository //  Added Streabuilder for Auth with Firebase + App Performance

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,34 @@
+// main.dart
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:kroenchen_app/config/themes.dart';
-import 'package:kroenchen_app/features/mainscreen/screens/bottom_navigation_bar_main.dart';
 import 'package:kroenchen_app/features/login/screens/loginpage.dart';
-import 'package:kroenchen_app/features/sign_up/screens/register_screen.dart';
-import 'package:kroenchen_app/features/sign_up/screens/register_screen_finish.dart';
-import 'package:kroenchen_app/features/sign_up/screens/register_screen_three.dart';
-import 'package:kroenchen_app/features/sign_up/screens/register_screen_two.dart';
-import 'package:kroenchen_app/features/welcome_screen/screens/welcome_screen.dart';
-import 'package:kroenchen_app/firebase_options.dart';
+// import 'package:kroenchen_app/features/welcome_screen/screens/welcome_screen.dart';
+import 'package:kroenchen_app/features/mainscreen/screens/bottom_navigation_bar_main.dart';
 import 'package:kroenchen_app/shared/repository/database_repository.dart';
-// import 'package:kroenchen_app/shared/repository/mock_database.dart';
 import 'package:kroenchen_app/shared/repository/shared_preferences_database.dart';
+import 'package:kroenchen_app/shared/repository/firebase_auth_repository.dart';
+import 'package:kroenchen_app/shared/repository/auth_repository.dart';
 import 'package:provider/provider.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(MultiProvider(providers: [
-    Provider<DatabaseRepository>(
-      create: (_) => SharedPreferencesDatabase(),
-    )
-  ], child: const MainApp()));
+
+  runApp(MultiProvider(
+    providers: [
+      Provider<DatabaseRepository>(
+        create: (_) => SharedPreferencesDatabase(),
+      ),
+      Provider<AuthRepository>(
+        create: (_) => FirebaseAuthRepository(),
+      )
+    ],
+    child: const MainApp(),
+  ));
 }
 
 class MainApp extends StatefulWidget {
@@ -38,26 +43,38 @@ class _MainAppState extends State<MainApp> {
 
   @override
   Widget build(BuildContext context) {
+    final authRepo = Provider.of<AuthRepository>(context, listen: false);
+
     return MaterialApp(
       // themeMode: ThemeMode.system,
       themeMode: isSwitched ? ThemeMode.light : ThemeMode.dark,
       theme: lightTheme,
       darkTheme: darkTheme,
-      home: const WelcomeScreen(),
+      home: StreamBuilder(
+        stream: authRepo.authStateChanges,
+        builder: (context, snapshot) {
+          final user = snapshot.data;
+          if (snapshot.connectionState == ConnectionState.active) {
+            if (user == null) {
+              return const LoginScreen();
+            } else {
+              return BottomNavigationBarMain(
+                isSwitched: isSwitched,
+                onChanged: (bool newValue) {
+                  setState(() {
+                    isSwitched = newValue;
+                  });
+                },
+              );
+            }
+          }
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
       routes: {
         '/loginscreen': (context) => const LoginScreen(),
-        '/registerscreenstart': (context) => const RegisterScreenStart(),
-        '/registerscreentwo': (context) => const RegisterScreenTwo(),
-        '/registerscreenthree': (context) => const RegisterScreenThree(),
-        '/registerscreenfinish': (context) => const RegisterScreenFinish(),
-        '/bottomnavigationbarmain': (context) => BottomNavigationBarMain(
-              isSwitched: isSwitched,
-              onChanged: (bool newValue) {
-                setState(() {
-                  isSwitched = newValue;
-                });
-              },
-            ),
       },
     );
   }
