6fc1b5a3e7af69a0c5b77871b469b27e7545d458
Dopa91 <code.dopatka@gmail.com>
2024-12-16 22:56:31 +0100

    Widget adjusted for use with firebase Auth

--- lib/shared/widgets/textfield_with_border.dart
+++ lib/shared/widgets/textfield_with_border.dart
@@ -5,15 +5,21 @@ class TextFieldBox extends StatelessWidget {
   const TextFieldBox({
     super.key,
     required this.text,
+    this.controller,
+    this.obscureText = false,
   });
 
   final String text;
+  final TextEditingController? controller;
+  final bool obscureText;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: TextField(
+        controller: controller,
+        obscureText: obscureText,
         decoration: InputDecoration(
           hintText: text,
           enabledBorder: OutlineInputBorder(
