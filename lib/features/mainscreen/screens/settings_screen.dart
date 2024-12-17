c521b345adcbfb9cee87eca7a3030c2991bd3691
Dopa91 <code.dopatka@gmail.com>
2024-12-17 02:03:16 +0100

    added:ignore_for_file: use_build_context_synchronously

--- lib/features/mainscreen/screens/settings_screen.dart
+++ lib/features/mainscreen/screens/settings_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:flutter/material.dart';
 import 'package:ionicons/ionicons.dart';
 // import 'package:kroenchen_app/config/colors.dart';
