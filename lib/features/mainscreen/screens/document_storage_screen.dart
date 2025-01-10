128d722e633fc48cc819743b04d2ffe2bd2bd7c0
Dopa91 <code.dopatka@gmail.com>
2025-01-10 12:39:10 +0100

    Little fix

--- lib/features/mainscreen/screens/document_storage_screen.dart
+++ lib/features/mainscreen/screens/document_storage_screen.dart
@@ -4,7 +4,6 @@ import 'package:kroenchen_app/config/colors.dart';
 import 'package:kroenchen_app/features/mainscreen/widgets/document_detail_screen.dart';
 import 'package:kroenchen_app/shared/models/document.dart';
 import 'package:kroenchen_app/shared/repository/database_repository.dart';
-import 'package:kroenchen_app/shared/repository/shared_preferences_database.dart';
 import 'package:kroenchen_app/shared/widgets/background_image_widget.dart';
 import 'package:provider/provider.dart';
 
