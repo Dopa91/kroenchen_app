048e8059d358e1dcde92efba45dc79f591c3a76b
Dopa91 <code.dopatka@gmail.com>
2025-01-10 12:25:26 +0100

    Adjusted Class Document

--- lib/shared/models/document.dart
+++ lib/shared/models/document.dart
@@ -10,4 +10,35 @@ class Document {
     required this.filePaths,
     required this.addedDate,
   });
+  Document copyWith({
+    String? id,
+    String? name,
+    List<String>? filePaths,
+    DateTime? addedDate,
+  }) {
+    return Document(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      filePaths: filePaths ?? this.filePaths,
+      addedDate: addedDate ?? this.addedDate,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name': name,
+      'filePaths': filePaths,
+      'addedDate': addedDate.toIso8601String(),
+    };
+  }
+
+  factory Document.fromJson(Map<String, dynamic> json) {
+    return Document(
+      id: json['id'],
+      name: json['name'],
+      filePaths: List<String>.from(json['filePaths']),
+      addedDate: DateTime.parse(json['addedDate']),
+    );
+  }
 }
