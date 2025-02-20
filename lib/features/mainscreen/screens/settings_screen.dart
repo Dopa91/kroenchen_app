8d6302fff6921a5e0aa5186f96410ee0cc214a60
Dopa91 <code.dopatka@gmail.com>
2025-02-20 12:46:02 +0100

    added toggle switch button, for Darkmode - due to design

--- lib/features/mainscreen/screens/settings_screen.dart
+++ lib/features/mainscreen/screens/settings_screen.dart
@@ -1,8 +1,11 @@
 // ignore_for_file: use_build_context_synchronously
+import 'dart:ui';
+
+import 'package:animated_toggle_switch/animated_toggle_switch.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:ionicons/ionicons.dart';
-// import 'package:kroenchen_app/config/colors.dart';
+import 'package:kroenchen_app/config/colors.dart';
 import 'package:kroenchen_app/features/mainscreen/widgets/profil_account_picture.dart';
 import 'package:kroenchen_app/shared/repository/database_repository.dart';
 import 'package:kroenchen_app/shared/widgets/background_image_widget.dart';
@@ -23,19 +26,17 @@ class SettingsScreen extends StatelessWidget {
   Future<void> logout(BuildContext context) async {
     final authRepo = Provider.of<AuthRepository>(context, listen: false);
     await authRepo.signOut();
-
     Navigator.pushNamedAndRemoveUntil(
         context, "/loginscreen", (route) => false);
   }
 
-  Future<void> _chanageProfilPicture(BuildContext context) async {
+  Future<void> _changeProfilPicture(BuildContext context) async {
     final picker = ImagePicker();
     final pickedFile = await picker.pickImage(source: ImageSource.gallery);
 
     if (pickedFile != null) {
       final db = Provider.of<DatabaseRepository>(context, listen: false);
       await db.saveProfilePicture(pickedFile.path);
-
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(content: Text("Profilbild erfolgreich aktualisiert!")),
       );
@@ -77,11 +78,25 @@ class SettingsScreen extends StatelessWidget {
           automaticallyImplyLeading: false,
           forceMaterialTransparency: true,
           actions: [
-            Switch(
-              value: isSwitched,
-              onChanged: onChanged,
+            Padding(
+              padding: const EdgeInsets.only(right: 16.0),
+              child: AnimatedToggleSwitch<bool>.dual(
+                current: isSwitched,
+                first: false,
+                second: true,
+                onChanged: onChanged,
+                style: ToggleStyle(
+                  backgroundColor: Colors.black12,
+                  indicatorColor:
+                      isSwitched ? buttonBlue : darkerBackgroundColorPurple,
+                  borderColor: Colors.grey.shade800,
+                ),
+                iconBuilder: (value) => value
+                    ? Icon(Icons.light_mode, color: sun)
+                    : Icon(Icons.dark_mode, color: moon),
+              ),
             ),
-          ], // todo: Darkmode!!! fix colors.
+          ],
         ),
         body: SafeArea(
           child: Padding(
@@ -91,27 +106,70 @@ class SettingsScreen extends StatelessWidget {
               children: [
                 const Expanded(
                   flex: 2,
-                  child: SizedBox(
-                    height: 16,
+                  child: SizedBox(height: 16),
+                ),
+                const ProfilAccountPicture(radius: 96),
+                const Expanded(
+                  flex: 4,
+                  child: SizedBox(height: 8),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white.withAlpha(50),
+                    borderRadius: BorderRadius.circular(16),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withAlpha(50),
+                        blurRadius: 10,
+                        spreadRadius: 2,
+                        offset: Offset(0, 4),
+                      ),
+                    ],
+                  ),
+                  padding: const EdgeInsets.all(16),
+                  child: Stack(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(16),
+                        child: BackdropFilter(
+                          filter: ImageFilter.blur(sigmaX: 0.42, sigmaY: 0.42),
+                          child: SizedBox(
+                            height: 69,
+                            width: double.infinity,
+                          ),
+                        ),
+                      ),
+                      Center(
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text(
+                              "Schön dich zu sehen!",
+                              textAlign: TextAlign.center,
+                              style: const TextStyle(
+                                color: Colors.black,
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              "Mina",
+                              textAlign: TextAlign.center,
+                              style: const TextStyle(
+                                color: Colors.black,
+                                fontSize: 36,
+                                fontWeight: FontWeight.w800,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-                const ProfilAccountPicture(
-                  radius: 96,
-                ),
-                Text(
-                  "Schön dich zu sehen!,",
-                  style: TextStyle(color: Colors.black, fontSize: 24),
-                ),
-                Text("Mina",
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold)),
                 const Expanded(
                   flex: 5,
-                  child: SizedBox(
-                    height: 16,
-                  ),
+                  child: SizedBox(height: 16),
                 ),
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 42),
@@ -123,13 +181,11 @@ class SettingsScreen extends StatelessWidget {
                         icon: Ionicons.people_outline,
                       ),
                       const SizedBox(height: 16),
-
                       MyIndividualButton(
                         newText: "Profilbild ändern",
-                        nextSite: () => _chanageProfilPicture(context),
+                        nextSite: () => _changeProfilPicture(context),
                         icon: Ionicons.image_outline,
                       ),
-
                       const SizedBox(height: 16),
                       MyIndividualButton(
                         newText: "Logout",
@@ -137,12 +193,6 @@ class SettingsScreen extends StatelessWidget {
                         icon: Ionicons.log_out_outline,
                       ),
                       const SizedBox(height: 16),
-                      // const MyIndividualButton(
-                      //   newText: "Konto Löschen",
-                      //   nextSite: null,
-                      //   icon: Ionicons.trash_bin_outline,
-                      // )
-                      // todo: imlemenmt function!!!
                     ],
                   ),
                 )
